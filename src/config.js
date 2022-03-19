271d8fba74daaaf35a18c48aec0fe7f91e3112c3
fhurtadoc <fabiohurtadoc@gmail.com>
2022-03-19 12:18:20 -0500

    ususario

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 let localhost={
     host:'bfoz3v67dffbceoi7uh3-mysql.services.clever-cloud.com',
-    user: 'bfoz3v67dffbceoi7uh3',
+    user: 'uzkgb0sreyuz1zuk',
     password:'hJeoZVXrod87L50OpcYX',
     database:'bfoz3v67dffbceoi7uh3',
     port:'3306'    
